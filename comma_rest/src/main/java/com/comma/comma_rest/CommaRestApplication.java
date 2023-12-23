86a506198a24f9bfc164ec5d9de93d7256149660
Martin <martin.zavacky@student.upjs.sk>
2023-12-23 13:55:14 +0100

    Initial commit

---
+++ comma_rest/src/main/java/com/comma/comma_rest/CommaRestApplication.java
@@ -0,0 +1,13 @@
+package com.comma.comma_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommaRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommaRestApplication.class, args);
+    }
+
+}
---
+++ comma_rest/src/test/java/com/comma/comma_rest/CommaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comma.comma_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommaRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
